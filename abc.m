ba7948d5757cda01d722693a437b42b6a8f880a3
Harshit Srivastava <58990472+hs7979@users.noreply.github.com>
2020-06-07 12:06:58 +0530

    Add files via upload

---
+++ abc.m
@@ -0,0 +1,35 @@
+clear;
+clc
+obj = imread( 'ball.JPG');
+imshow ( obj )
+
+red = obj (:,:,1);
+green = obj(:,:,2);
+blue = obj(:,:,3);
+
+figure (1)
+subplot (2, 2, 1) ; imshow (obj); title ( 'Original Image ' ) ;
+subplot (2, 2, 2) ; imshow (red); ('Red Plane');
+subplot (2 , 2, 3); imshow (green); title ( 'Green Plane');
+subplot (2, 2, 4) ; imshow (blue); title( 'Blue Plane');
+
+figure (2)
+level = 0.37;
+bw2 = imbinarize (blue, level) ;
+subplot (2, 2, 1); imshow (bw2); title ( 'Blue plane thresholded') ;
+
+fill = imfill(bw2,'holes');
+subplot(2,2,2); imshow(fill);title('Holes Filled');
+
+clear = imclearborder(fill);
+subplot(2,2,3); imshow(clear); title('Remove blobs on border');
+
+se = strel('disk',7);
+open = imopen(fill,se);
+subplot(2,2,4);imshow(open);title('Remove small blobs');
+
+diameter  =regionprops(open,'MajorAxisLength')
+
+figure(3)
+imshow(obj)
+d= imdistline;
